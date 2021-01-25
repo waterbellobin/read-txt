e4efc22722b8ed6b36d7e7585280ef0bddd50e54
waterbellobin <lgjong951@naver.com>
2021-01-25 14:23:46 +0900

    basic format

--- read_vacuum_data.py
+++ read_vacuum_data.py
@@ -1,1 +1,11 @@
-import numpy
+import numpy as np
+
+array_from_file = np.loadtxt("C:/Users/nosquest17/Desktop/Sujong/daily_works/20210125_read_vacuum_data/TB30Pressure-2021-01-22.log", dtype=str)
+
+print(len(array_from_file))
+time_high_vacuum = np.zeros(shape=(len(array_from_file),2))
+
+#time_high_vacuum = array_from_file[:,0]
+
+print(time_high_vacuum)
+print(array_from_file[0][1])
